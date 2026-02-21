03554ddd38e17b5fe805e3182c9628386d7e6a66
DavesCodingPortfolio <davestablet091@gmail.com>
2026-02-21 13:54:40 +0000

    added request method for get and post

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
 def index(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    if request.method == "POST":
+        return HttpResponse("You have just POSTed something")
+    else:
+        return HttpResponse(request.method)
